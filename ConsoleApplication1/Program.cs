46a7a4231b6f68f7f4220aa7cf17df023b3dcf27
ajay18 <ajreddy18@gmail.com>
2016-07-12 07:32:44 -0400

    Add files via upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+            
+            string s = "AFRTSSDJJJ";
+            foreach (char c in s)
+            {
+                if (c == 'T')
+                {
+                    continue;
+                }
+                else if(c == 'D')
+                {
+                    break;
+                }
+
+                Console.WriteLine(c);
+
+            }
+            Console.Read();
+
+        }
+    }
+}
